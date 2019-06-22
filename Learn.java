efd5fe3344768ba386e5bf86096b6d3f444f5289
RuslanLozinskiy <ruslanlozinskiy0@gmail.com>
2019-06-22 10:42:52 +0300

    Add

---
+++ Learn.java
@@ -0,0 +1,96 @@
+package ruslan.com;
+
+import java.util.Scanner;
+
+public class Learn {
+//commit
+	public static void main(String[] args) {
+//		
+//		 int a = 5;//
+//		 int b =5;//
+//System.out.println(a+b);//
+//		 byte m = 5;//
+//		 byte q = 2;//
+//System.out.println(m*q);//
+//		 int k = 100;//
+//		 int w = 5;//
+//System.out.println(k/w);//
+//		 int e = 123;//
+//		 int r = 113;//
+//System.out.println(e-r);//
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("ßê òåáå çâàòè? ");
+//		String name = sc.nextLine();
+//		System.out.println("òåáå çâàòè "+ name);
+//		System.out.print("Ñê³ëüêè òîá³ ğîê³â?");
+//		String age = sc.next();
+//		System.out.println("òîá³ "+ age + " ğîê³â");
+	sdfsdfd	
+		
+		
+		 //                               HOMEWORK
+	//	--------------------------------------------------------------------------------
+   //TASK¹2
+//Scanner sc = new Scanner(System.in);
+//		System.out.println("ßê òåáå çâàòè? ");
+//String name = sc.nextLine();
+//		System.out.println("Òåáå çâàòè "+ name);
+//		System.out.println("------------");
+//		System.out.println("Äå òè æèâåø? ");
+//String street = sc.nextLine();
+//		System.out.println("Òè æèâåø â " + street);
+		
+//	TASK¹3	
+//byte t1 = 15;
+//byte t2 = 10;
+//byte t3 = 5;
+//byte together = (byte) (t1 + t2 + t3);
+//		System.out.println("Âîíè ğîçìîâëÿëè ğàçîì " + together + " õâèëèí");
+//
+//	TASK¹1	
+//int radius = 20;
+//		System.out.println("Ğàä³óñ ñòîâáà = " + radius);
+//int diameter= 40;
+//		System.out.println("Ä³ìåòğ = " + diameter);
+//double lenght = 3.14159265*2*20;
+//		System.out.println("Äîâæèíà = " + lenght );
+		
+		// Íàïèñàòè ïğîãğàìó êîìïşòåğ ìàøèíè
+//		Scanner sc = new Scanner(System.in);
+//System.out.println("Äîáğèé äåíü âè çàéøëè â ìåíş ùî âè õî÷èòå çğîáèòè?");
+//		String display = sc.nextLine();
+//		System.out.println("Â³òàş, âè çàéøëè â ãîëîâíèé äèñïëåé");
+//		System.out.println("Ìåíş");
+//		System.out.println("GPS");
+//		System.out.println("àóä³î");
+//		String menu = sc.nextLine();
+//		System.out.println("Â³òàş âè çàéøëè â ìåíş" + menu);
+//		String gps = sc.nextLine();
+//		System.out.println("Â³òàş, âè çàéøëè â GPS" + gps);
+//		String audio = sc.nextLine();
+//		System.out.println("Â³òàş, âè çàéøëè â àóä³î" + audio);
+//		try {
+//	        Byte b1 = new Byte("10");
+//	        System.out.println(b1);
+//	    } catch (NumberFormatException e) {
+//	        System.err.println("Íåâåğíûé ôîğìàò ñòğîêè!");
+//double d = 2.5;
+//int i = (int) d;
+//System.out.println(i);
+//int b = 5;
+//double k = (int) b;
+//System.out.println(k);
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Äîáğèé äåíü, öå âàø ÷åê");
+//		String name = sc.nextLine();
+//	    System.out.println("Âàøå ³ìÿ---> " + name);
+//	    String sum = sc.nextLine();
+//	    System.out.println("Âàøà ñóìà òàêà --->" + sum);
+	    
+	}
+	}
+
+		
+	
+
+
