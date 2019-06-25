554c4a3ad539233b678801b8ec978be5b48abf12
mikiyur <mikiyur@gmail.com>
2019-06-25 12:18:42 +0300

    Made changes by commented

--- hw1/HomeWork1a.java
+++ hw1/HomeWork1a.java
@@ -8,20 +8,18 @@ public class HomeWork1a {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		double r;
-		double l;
+		int r;
+		double p;
 		double s;
-		double pi;
-		pi = 3.14;
 
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-System.out.print("Введіть радіус = ");
- r = Integer.parseInt(br.readLine());
- l = 2*(pi*r);
- s = (pi*r)*(pi*r);
- 
- System.out.println("Довжина кола = "+l);
- System.out.print("Площа кола = "+s);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter the radius = ");
+		r = Integer.parseInt(br.readLine());
+		p = 2 * (Math.PI * r);
+		s = (Math.PI * r) * (Math.PI * r);
+
+		System.out.println("Perimeter = " + p);
+		System.out.println("Area = " + s);
 	}
 
 }
--- hw1/HomeWork1c.java
+++ hw1/HomeWork1c.java
@@ -6,28 +6,28 @@ import java.io.InputStreamReader;
 
 public class HomeWork1c {
 
-	public static void main(String[] args)throws IOException {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-int c1;
-int c2;
-int c3;
-int t1;
-int t2;
-int t3;
-c1=1;
-c2=2;
-c3=3;
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-System.out.println("¬вед≥ть t1");
- t1 = Integer.parseInt(br.readLine());
- System.out.println("¬вед≥ть t2");
- t2 = Integer.parseInt(br.readLine());
- System.out.println("¬вед≥ть t3");
- t3 = Integer.parseInt(br.readLine());
- System.out.println("Time 1 = "+ (c1*t1));
- System.out.println("Time 2 = "+ (c2*t2));
- System.out.println("Time 3 = "+ (c3*t3));
- System.out.println("Time oll = "+ (c1*t1+c2*t2+c3*t3));
+		int c1;
+		int c2;
+		int c3;
+		int t1;
+		int t2;
+		int t3;
+		c1 = 1;
+		c2 = 2;
+		c3 = 3;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter t1");
+		t1 = Integer.parseInt(br.readLine());
+		System.out.println("Enter t2");
+		t2 = Integer.parseInt(br.readLine());
+		System.out.println("Enter t3");
+		t3 = Integer.parseInt(br.readLine());
+		System.out.println("Calculation 1 = " + (c1 * t1));
+		System.out.println("Calculation 2 = " + (c2 * t2));
+		System.out.println("Calculation 3 = " + (c3 * t3));
+		System.out.println("Calculation oll = " + (c1 * t1 + c2 * t2 + c3 * t3));
 	}
 
 }
