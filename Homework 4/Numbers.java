25e06472d9c508dd56496383dfa96de8fa4bc413
StepanMarynchuk <marynchuks@gmail.com>
2019-07-07 23:57:08 +0300

    add new Numbers

--- Homework 4/Numbers.java
+++ Homework 4/Numbers.java
@@ -17,22 +17,25 @@ public class Numbers {
 
 		System.out.println("Enter number 3:");
 		int n3 = Integer.parseInt(br.readLine());
-		if (n1 > n2 && n1 > n3) {
-			System.out.println("Max number is:" + " " + n1);
-		} else if (n2 > n1 && n2 > n3) {
-			System.out.println("Max number is:" + " " + n2);
-		} else
-			System.out.println("Max number is:" + " " + n3);
+//		if (n1 > n2 && n1 > n3) {
+//			System.out.println("Max number is:" + " " + n1);
+//		} else if (n2 > n1 && n2 > n3) {
+//			System.out.println("Max number is:" + " " + n2);
+//		} else
+//			System.out.println("Max number is:" + " " + n3);
+//
+//		if (n1 < n2 && n1 < n3) {
+//			System.out.println("Min number is:" + " " + n1);
+//		} else if (n2 < n1 && n2 < n3) {
+//			System.out.println("Min number is:" + " " + n2);
+//		} else
+//		System.out.println("Min number is:" + " " + n3);
 
-		if (n1 < n2 && n1 < n3) {
-			System.out.println("Min number is:" + " " + n1);
-		} else if (n2 < n1 && n2 < n3) {
-			System.out.println("Min number is:" + " " + n2);
-		} else
-		System.out.println("Min number is:" + " " + n3);
-
-		//double max = (n1 > n2)? n1;
-		//System.out.println(max);
+		double max = (n1 > n2 && n1 > n3)?n1: (n2>n3)?n2:n3;
+		System.out.println("Max" + "-"+max);
+		
+		double min = (n1 < n2 && n1 < n3)?n1: (n2<n3)?n2:n3;
+		System.out.println("Min" + "-"+min);
 		//return Math.min(n1, Math.min(n2,n3)));
 	
 	}
