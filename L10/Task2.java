2d7843ac9687a3f68626770cb5d71b85adfcc608
Stepan Marynchuk <smarytc@training.local>
2019-07-18 20:41:09 +0300

    add L10

---
+++ L10/Task2.java
@@ -0,0 +1,21 @@
+package less.ons10;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+
+		try {
+			for (int i = 0; i < 5; i++) {
+				Thread.sleep(2000);
+				System.out.println("Hello, world");
+				Thread.sleep(3000);
+				System.out.println("Peace in the peace");
+
+			} 
+				System.out.println("My name is Stepan");
+			
+		} catch (InterruptedException e) {
+
+		}
+	}
+}
