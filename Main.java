4ffe657199f0e8b8e117e6b1c5f98221210939f2
Pavlo Boianovskyi <pavloboianovskii@gmail.com>
2019-07-30 18:27:59 +0300

    main

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.io.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("input task code ");
+        int code = Integer.parseInt(br.readLine());
+        switch(code){
+            case 1:
+                break;
+        }
+    }
+}
+
+
